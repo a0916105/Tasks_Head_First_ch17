1d1c353f6fa52d7318f7d607fbe34cc60de61ad1
a0916105 <whdofjew@gmail.com>
2023-02-07 17:08:52 +0800

    使用viewModelScope.launch在view model的作用域啟動協同程序

--- app/src/main/java/tw/idv/jew/tasks/TasksViewModel.kt
+++ app/src/main/java/tw/idv/jew/tasks/TasksViewModel.kt
@@ -1,13 +1,17 @@
 package tw.idv.jew.tasks
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 class TasksViewModel(val dao: TaskDao) : ViewModel() {
     var newTaskName = ""    //用來儲存工作名稱
 
     fun addTask(){
-        val task = Task()
-        task.taskName = newTaskName
-        dao.insert(task)    //加入資料庫
+        viewModelScope.launch {//在view model的作用域啟動協同程序
+            val task = Task()
+            task.taskName = newTaskName
+            dao.insert(task)    //加入資料庫
+        }
     }
 }
