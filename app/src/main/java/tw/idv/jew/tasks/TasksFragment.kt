deceaf54d77689187c4111f5780201489d51a3b5
a0916105 <whdofjew@gmail.com>
2023-02-21 14:50:21 +0800

    在TasksFragment中，增加建立資料庫的程式碼

--- app/src/main/java/tw/idv/jew/tasks/TasksFragment.kt
+++ app/src/main/java/tw/idv/jew/tasks/TasksFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import tw.idv.jew.tasks.databinding.FragmentTasksBinding
+import androidx.lifecycle.ViewModelProvider
 
 class TasksFragment : Fragment() {
     private var _binding: FragmentTasksBinding? = null
@@ -17,6 +19,14 @@ class TasksFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentTasksBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        //建立資料庫（如果它還不存在）並取得taskDao屬性的參考
+        val application = requireNotNull(this.activity).application
+        val dao = TaskDatabase.getInstance(application).taskDao
+        //取得viewModel
+        val viewModelFactory = TasksViewModelFactory(dao)
+        val viewModel = ViewModelProvider(
+            this, viewModelFactory).get(TasksViewModel::class.java)
 
         return view
     }
