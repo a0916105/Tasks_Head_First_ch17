8afc1b756efacd0cc9bd509969a7f8721f339811
a0916105 <whdofjew@gmail.com>
2023-02-22 18:26:20 +0800

    修改TasksViewModel

--- app/src/main/java/tw/idv/jew/tasks/TasksViewModel.kt
+++ app/src/main/java/tw/idv/jew/tasks/TasksViewModel.kt
@@ -1,6 +1,5 @@
 package tw.idv.jew.tasks
 
-import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
@@ -8,10 +7,7 @@ import kotlinx.coroutines.launch
 class TasksViewModel(val dao: TaskDao) : ViewModel() {
     var newTaskName = ""    //用來儲存工作名稱
 
-    private val tasks = dao.getAll()    //從資料庫取得記錄
-    val tasksString = Transformations.map(tasks) {
-            tasks -> formatTasks(tasks) //將tasks(LiveData<List<Task>>)轉換成LiveData<String>
-    }
+    val tasks = dao.getAll()    //從資料庫取得記錄
 
     fun addTask(){
         viewModelScope.launch {//在view model的作用域啟動協同程序
@@ -19,18 +15,5 @@ class TasksViewModel(val dao: TaskDao) : ViewModel() {
             task.taskName = newTaskName
             dao.insert(task)    //加入資料庫
         }
-    }
-
-    fun formatTasks(tasks: List<Task>): String {
-        return tasks.fold("") {
-                str, item -> str + '\n' + formatTask(item)
-        }
-    }
-
-    fun formatTask(task: Task): String {
-        var str = "ID: ${task.taskId}"
-        str += '\n' + "Name: ${task.taskName}"
-        str += '\n' + "Complete: ${task.taskDone}" + '\n'
-        return str
     }
 }
