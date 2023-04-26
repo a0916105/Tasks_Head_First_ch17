059f80a83d547fbad47e56156ba2c867495070a2
a0916105 <whdofjew@gmail.com>
2023-04-26 10:20:21 +0800

    在adapter的view holder中，使用binding類別來充氣layout

--- app/src/main/java/tw/idv/jew/tasks/TaskItemAdapter.kt
+++ app/src/main/java/tw/idv/jew/tasks/TaskItemAdapter.kt
@@ -2,11 +2,9 @@ package tw.idv.jew.tasks
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.CheckBox
-import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import tw.idv.jew.tasks.databinding.TaskItemBinding
 
 class TaskItemAdapter : ListAdapter<Task, TaskItemAdapter.TaskItemViewHolder>(TaskDiffItemCallback()) { //為了處理串列並搭配DiffUtil使用，改繼承ListAdapter
     //將layout充氣並回傳view holder，當APP需要建立view holder時會呼叫它。parent是指recycler view
@@ -18,25 +16,22 @@ class TaskItemAdapter : ListAdapter<Task, TaskItemAdapter.TaskItemViewHolder>(Ta
         holder.bind(item)
     }
 
-    class TaskItemViewHolder(val rootView: CardView)
-        : RecyclerView.ViewHolder(rootView) {
-        val taskName = rootView.findViewById<TextView>(R.id.task_name)
-        val taskDone = rootView.findViewById<CheckBox>(R.id.task_done)
+    class TaskItemViewHolder(val binding: TaskItemBinding)  //為了使用binding類別來充氣layout，所以改成接收binding物件
+        : RecyclerView.ViewHolder(binding.root) {   //binding.root是layout的根view
 
         companion object {
             //建立每一個view holder並充氣它的layout
             fun inflateFrom(parent: ViewGroup): TaskItemViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater
-                    .inflate(R.layout.task_item, parent, false) as CardView
-                return TaskItemViewHolder(view)
+                //改用binding方式來將layout充氣
+                val binding = TaskItemBinding.inflate(layoutInflater, parent, false)
+                return TaskItemViewHolder(binding)  //使用binding變數來建立ViewHolder
             }
         }
         //將資料加入view holder的layout
         fun bind(item: Task) {
-            //將工作名稱加入layout的根view
-            taskName.text = item.taskName
-            taskDone.isChecked = item.taskDone
+            //設定layout的data binding變數
+            binding.task = item
         }
     }
 }
