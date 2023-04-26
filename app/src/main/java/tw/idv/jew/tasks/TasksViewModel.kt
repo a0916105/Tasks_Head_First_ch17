c58f7fd0b256c0078065a39d9f3d2c03c60ebea9
a0916105 <whdofjew@gmail.com>
2023-04-26 15:11:16 +0800

    在ViewModel加入一個live data讓ViewModel有能力可以決定何時前往下一個Fragment

--- app/src/main/java/tw/idv/jew/tasks/TasksViewModel.kt
+++ app/src/main/java/tw/idv/jew/tasks/TasksViewModel.kt
@@ -1,5 +1,7 @@
 package tw.idv.jew.tasks
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
@@ -9,6 +11,11 @@ class TasksViewModel(val dao: TaskDao) : ViewModel() {
 
     val tasks = dao.getAll()    //從資料庫取得記錄
 
+    private val _navigateToTask = MutableLiveData<Long?>()
+    val navigateToTask: LiveData<Long?>
+        get() = _navigateToTask
+
+
     fun addTask(){
         viewModelScope.launch {//在view model的作用域啟動協同程序
             val task = Task()
@@ -16,4 +23,13 @@ class TasksViewModel(val dao: TaskDao) : ViewModel() {
             dao.insert(task)    //加入資料庫
         }
     }
+
+    fun onTaskClicked(taskId: Long) {
+        _navigateToTask.value = taskId
+    }
+
+    fun onTaskNavigated() {
+        _navigateToTask.value = null
+    }
+
 }
